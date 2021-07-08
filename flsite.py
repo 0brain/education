23cd93f1fb0e7f1a3f8f96b8107e7b88fe529b55
nazar <nazar.khort@gmail.com>
2021-07-08 15:51:08 +0300

    Вніс зміни в flsite.py. Імпортував функцію render_template. Підключив її виклик в момент переходу /index, /, /aboutта передав відповідні назви шаблонів (index.html і about.html) для читання.

--- flsite.py
+++ flsite.py
@@ -1,5 +1,8 @@
-'''1) З бібліотеки flask імпортуємо клас Flask'''
-from flask import Flask
+'''1) З бібліотеки flask імпортуємо клас Flask
+   8) З бібліотеки flask імпортуємо функцію render_template, яка приймає ім’я шаблону
+   і список змінних аргументів шаблону, а повертає готовий шаблон з заміненими аргументами
+'''
+from flask import Flask, render_template
 
 '''2) У випадку імпорту __name__ - буде ім’ям поточного файлу'''
 app = Flask(__name__)
@@ -8,15 +11,16 @@ app = Flask(__name__)
    7) def index() - повертає повідомлення, яке буде відображено в браузері за URL адресою "/" та "index"
 '''
 
+
 @app.route("/index")
 @app.route("/")
 def index():
-    return "index"
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return "<h1>Про сайт</h1>"
+    return render_template("about.html")
 
 
 '''3) У випадку самостійного запуску __name__ - буде мати значення __main__;
