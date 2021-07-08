79d6f217bf45f0077b0ac3c7e3707c8958a07601
nazar <nazar.khort@gmail.com>
2021-07-09 00:50:39 +0300

    Додав методи POST i GET, імпортував модуль request

--- flsite.py
+++ flsite.py
@@ -2,7 +2,7 @@
    8) З бібліотеки flask імпортуємо функцію render_template, яка приймає ім’я шаблону
    і список змінних аргументів шаблону, а повертає готовий шаблон з заміненими аргументами
 '''
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__) # 2) У випадку імпорту __name__ - буде ім’ям поточного файлу
 
@@ -27,7 +27,7 @@ def about():
     return render_template("about.html", title="Про сайт", menu=menu)  # 10) в функцію render_template крім назви шаблону передаємо параметр title
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['POST', 'GET'])
 def contact():
     return render_template("contact.html", title="Зворотній зв’язок", menu=menu)
 
